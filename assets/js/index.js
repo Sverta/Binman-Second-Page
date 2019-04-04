34b47c39c0c87ee8be6bab77c142b201297de6f5
Kuchynskaya <sbaidak@scnsoft.com>
2019-04-04 15:45:25 +0300

    change hover for links and buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 function toggle_visibility(id) {
-   var e = document.getElementById(id);
-   if(e.style.display == 'block')
-    e.style.display = 'none';
-  else
-    e.style.display = 'block';
+  if (window.screen.width < 991){
+     var e = document.getElementById(id);
+     if(e.style.display == 'block')
+      e.style.display = 'none';
+    else
+      e.style.display = 'block';
+  }
 }
 
 
