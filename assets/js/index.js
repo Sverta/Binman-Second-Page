7656dd17500d73238120d9cd33ade6cfc3690c23
Kuchynskaya <sbaidak@scnsoft.com>
2019-04-04 12:21:30 +0300

    Fix font, svg, and problem with images

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,4 @@
-// var ajax = new XMLHttpRequest();
-// ajax.open("GET", "sprites/sprite.svg", true);
-// ajax.send();
-// ajax.onload = function(e) {
-//   var div = document.createElement("div");
-//   div.innerHTML = ajax.responseText;
-//   document.body.insertBefore(div, document.body.childNodes[0]);
-// }
-
- function toggle_visibility(id) {
+function toggle_visibility(id) {
    var e = document.getElementById(id);
    if(e.style.display == 'block')
     e.style.display = 'none';
